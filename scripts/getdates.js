25593cc274255111d16508eba8f884c466e7ebf7
Marisabel Trejo <97261405+MarisabelTrejo@users.noreply.github.com>
2024-05-08 15:11:09 -0500

    Yet to fix map

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -9,3 +9,11 @@ let LastModif = new Date(document.lastModified);
 lastUpdated.textContent = LastModif;
 
 Year.textContent += presentYear;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
