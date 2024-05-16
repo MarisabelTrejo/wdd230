7642b3b67daaad30be87fbac4d8e71de7712c352
Marisabel Trejo <97261405+MarisabelTrejo@users.noreply.github.com>
2024-05-16 14:38:06 -0500

    chamber site progress

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,14 @@
+const lastUpdated = document.getElementById("modified");
+const Year = document.getElementById("year");
+
+const d = new Date();
+let presentYear = d.getFullYear();
+
+let LastModif = new Date(document.lastModified);
+
+lastUpdated.textContent = LastModif;
+
+Year.textContent += presentYear;
+
+const navigation = document.querySelector('.navigation');
+
