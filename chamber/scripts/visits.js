e475cab314c263d9ba60cf1cbf330e8c10145992
Marisabel Trejo <97261405+MarisabelTrejo@users.noreply.github.com>
2024-06-08 14:18:57 -0500

    visits done

---
+++ chamber/scripts/visits.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // 1. Check for existing counter in localStorage
+    let visitCount = localStorage.getItem('visits');
+
+    // 2. Initialize or update the counter
+    if (visitCount === null) {
+        visitCount = 0;
+    } else {
+        visitCount = parseInt(visitCount);
+    }
+    visitCount++;
+
+    // 3. Display the counter in the visit-counter div
+    document.getElementById('visits').innerText = `visits:  ${visitCount}`;
+
+    // 4. Save the updated counter back to localStorage
+    localStorage.setItem('visits', visitCount);
+
+});
